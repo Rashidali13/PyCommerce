f321a22e5388ef096584ef1f55bc2e2a67b9b76b
Rashid ali <codefaculty3@gmail.com>
2022-08-24 16:59:47 +0500

    User registration completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -34,8 +35,23 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            # messages.success(request, 'Your are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid Email or Password.')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 
+@login_required(login_url='login')
 def logout(request):
-    return
+    auth.logout(request)
+    messages.success(request, 'Your are logged out.')
+    return redirect('login')
