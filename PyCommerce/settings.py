89b6d27165b11fca55ac79be6b85cf678916150e
Rashid ali <codefaculty3@gmail.com>
2022-10-12 14:53:15 +0500

    email template updated

--- PyCommerce/settings.py
+++ PyCommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -135,7 +136,14 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-from django.contrib.messages import constants as messages
+
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# SMTP CONFIGRATION
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ameliawatson357@gmail.com'
+EMAIL_HOST_PASSWORD = 'Samali109@'
+EMAIL_USE_TLS = True
